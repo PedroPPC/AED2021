f1bd0760a9ccbce8f56ea421557e40b9672350f0
Pedro Páez <pedro.paez@epn.edu.ec>
2021-08-05 22:29:52 -0500

    Manejo de Archivos

---
+++ Manejo Archivos/cmake-build-debug/CMakeFiles/Ordenamiento_por_burbuja.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ordenamiento_por_burbuja.dir/main.cpp.obj"
+  "Ordenamiento_por_burbuja.exe"
+  "Ordenamiento_por_burbuja.exe.manifest"
+  "Ordenamiento_por_burbuja.pdb"
+  "libOrdenamiento_por_burbuja.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ordenamiento_por_burbuja.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
